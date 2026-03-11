030e4bd88ca75e24d2d0419a0b7edb3e385a53af
Svitlana <missis.chuvaeva@ukr.net>
2026-03-11 19:45:43 +0000

    add navigation in header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,17 +1,28 @@
 // components/Header/Header.tsx
 
 import css from './Header.module.css';
+import Link from 'next/link';
 
 const Header = () => {
   return (
     <header className={css.header}>
-      {/* <h2></h2> */}
-      <nav>
+      <Link href='/' aria-label='Home'>
+        LOgo
+      </Link>
+      <nav aria-label='Main Navigation'>
         <ul className={css.navigation}>
-          <li>Про нас</li>
-          <li>Послуги</li>
-          <li>Портфоліо</li>
-          <li>Контакти</li>
+          <li>
+            <Link href='/about'>Про нас</Link>
+          </li>
+          <li>
+            <Link href='/services'>Послуги</Link>
+          </li>
+          <li>
+            <Link href='/portfolio'>Портфоліо</Link>
+          </li>
+          <li>
+            <Link href='/contacts'>Контакти</Link>
+          </li>
         </ul>
       </nav>
     </header>
