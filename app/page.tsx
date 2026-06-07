eb8cc37e7828850aec4eb83e034f3ab3966eaf2d
Svitlana <missis.chuvaeva@ukr.net>
2026-06-07 21:12:04 +0100

    add WorkPlan components with styles (for mobile, tablet,desktop), add workPlan type and constant

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import Hero from '@/components/Hero/Hero';
 import AboutMain from '@/components/AboutMain/AboutMain'
 import ServiceMain from '@/components/ServiceMain/ServiceMain'
-
+import WorkPlan from '@/components/WorkPlan/WorkPlan'
 export default function Home() {
   return (
     <>
       <Hero />
       <AboutMain />
-      <ServiceMain/>
+      <ServiceMain />
+      <WorkPlan/>
     </>
   );
 }
